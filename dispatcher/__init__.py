12cecac5ea2a9039cf4a65192e5bb5d1d1b0e2f1
Imad Dodin <imad.dodin@mail.mcgill.ca>
2020-04-24 20:46:21 -0400

    Update CLI

--- dispatcher/__init__.py
+++ dispatcher/__init__.py
@@ -1,5 +1,8 @@
 import argparse
-from parsing import generate
+
+from graphs import Edge, enhancer
+from parsing import generate, write_result
+
 
 class Dispatcher(object):
     def __init__(self):
@@ -31,23 +34,43 @@ class Dispatcher(object):
         args = parser.parse_args()
 
         try:
-            self.rel_goal = args.reliability
+            self.rel_goal = args.reliability[0]
         except AttributeError as e:
             self.rel_goal = 0
 
         try:
-            self.cost_goal = args.cost
+            self.cost_goal = args.cost[0]
         except AttributeError as e:
             self.cost_goal = 1000000
 
         getattr(self, args.command)(args.input, args.output)
 
-
     def reliability(self, input_file, output_file):
-        print("Attempting to generate network meeting Reliability Goal {}".format(self.rel_goal))
-        print(generate(input_file))
-
+        print("Attempting to generate Network meeting Reliability Goal: {} ...".format(self.rel_goal))
+        network = generate(input_file)
+        max_reliability = network.compute_reliability()
+        if max_reliability > self.rel_goal:
+            self.print_output(network)
+            write_result(network, output_file)
+        else:
+            print("Could not find Network satisfying Reliability Goal!")
+            output_file.write("N/A")
 
     def cost(self, input_file, output_file):
-        print("Attempting to generate network with Maximal Reliability meeting Cost Goal {}".format(self.cost_goal))
-        print(generate(input_file))
+        print("Attempting to generate network with Maximal Reliability under Cost Constraint: {} ...".format(self.cost_goal))
+        network = generate(input_file)
+        cost_mst = network.find_mst(Edge.get_cost)
+        maximized = enhancer.enhance_cost(network, cost_mst, self.cost_goal)
+        if maximized:
+            self.print_output(maximized)
+            write_result(maximized, output_file)
+        else:
+            print("There is no connected Network meeting Cost Goal!")
+            output_file.write("N/A")
+
+    @staticmethod
+    def print_output(network):
+        print("Found Network satisfying Reliability Goal!")
+        print("Reliability: {}".format(network.compute_reliability()))
+        print("Cost: {}".format(network.compute_cost()))
+        print(network.edges)
