0af206ac60f05ecb34b7a3943befcbf4039232f5
Imad Dodin <imad.dodin@mail.mcgill.ca>
2020-04-24 21:10:16 -0400

    Improve Command Line Parsing

--- dispatcher/__init__.py
+++ dispatcher/__init__.py
@@ -7,39 +7,42 @@ from parsing import generate, write_result
 class Dispatcher(object):
     def __init__(self):
         parser = argparse.ArgumentParser(description='Generate a Network Design')
-        parser.add_argument('--input', '-i', nargs=1, action='store', default='input.txt', type=argparse.FileType('r'),
+        parser.add_argument('--input', '-i', nargs='?', action='store', default='input.txt', type=argparse.FileType('r'),
                             help='Input file containing symmetric reliabilities then costs in row major form '
                                  '(default: input.txt)'
                             )
 
-        parser.add_argument('--output', '-o', nargs=1, action='store', default='output.txt',
+        parser.add_argument('--output', '-o', nargs='?', action='store', default='output.txt',
                             type=argparse.FileType('w'), help='Output file containing selected edges'
                                                               ' (default: output.txt)'
                             )
 
-        subparsers = parser.add_subparsers(help='Sub-command help', dest='command', required=True)
+        subparsers = parser.add_subparsers(help='reliability - Generate a network design meeting the given '
+                                                'reliability goal (does not minimize cost).\n\ncost - Generate a '
+                                                'network design meeting the given cost constraint, maximizing '
+                                                'reliability.', dest='command', required=True)
 
         parser_rel = subparsers.add_parser('reliability', description='Generate a network design meeting the given '
-                                                                      'reliability goal (not necessarily, minimum cost)'
+                                                                      'reliability goal (does not minimize cost).'
                                            )
-        parser_rel.add_argument('reliability', type=float, nargs=1,
+        parser_rel.add_argument('reliability', type=float, nargs='?',
                                 help="Reliability Goal for this design (must be between 0 and 1)")
 
         parser_cost = subparsers.add_parser('cost', description='Generate a network design meeting the given cost '
                                                                 'constraint, with maximal reliability.')
 
-        parser_cost.add_argument('cost', type=int, nargs=1,
+        parser_cost.add_argument('cost', type=int, nargs='?',
                                  help="Cost Constraint")
 
         args = parser.parse_args()
 
         try:
-            self.rel_goal = args.reliability[0]
+            self.rel_goal = args.reliability
         except AttributeError:
             self.rel_goal = 0
 
         try:
-            self.cost_goal = args.cost[0]
+            self.cost_goal = args.cost
         except AttributeError:
             self.cost_goal = 1000000
 
